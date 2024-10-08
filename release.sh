2648f0d15acfbcc7f4786c69377440652a20c0a8
Mitchell R <github@mrincworld.com>
2024-10-08 23:29:13 +0200

    more workpress flunkieness

--- release.sh
+++ release.sh
@@ -15,5 +15,5 @@ mkdir -p $DIST_DIR
 
 # Change to the build directory and create the zip file
 pushd $BUILD_DIR
-zip -r $DIST_DIR/betterportal-theme-embedded-$VERSION.zip .
+zip -r $DIST_DIR/betterportal-theme-embedded-v$VERSION.zip .
 popd
